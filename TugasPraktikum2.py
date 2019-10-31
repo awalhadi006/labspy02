7e24dc757d05e9077389c392a9a6304e5fe937ec
awalhadi006 <awalhadi006@gmail.com>
2019-11-01 01:53:17 +0700

    Tugas Praktikum 2

--- TugasPraktikum2.py
+++ TugasPraktikum2.py
@@ -2,7 +2,7 @@ a = int(input("Masukkan bilangan a: "))
 b = int(input("Masukkan bilangan b: "))
 c = int(input("Masukkan bilangan c: "))
 
-if a > c and a > b:
+if a > b and a > c:
     print("Bilangan terbesar adalah bilangan a =",a)
 elif b > a and b > c:
     print("Bilangan terbesar adalah bilangan b =",b)
